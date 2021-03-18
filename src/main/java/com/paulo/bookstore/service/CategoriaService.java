241399b262d7357789735d9042873582bced4c83
Paulojosep <paulo.jmodesto@htmail.com>
2021-03-18 18:04:24 -0300

    GET para Categoria

---
+++ src/main/java/com/paulo/bookstore/service/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.paulo.bookstore.service;
+
+import com.paulo.bookstore.domain.Categoria;
+import com.paulo.bookstore.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repository;
+
+    public Categoria findById(Integer id) {
+        Optional<Categoria> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
